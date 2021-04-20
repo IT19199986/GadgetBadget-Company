2dc3b2c7c419bf4cda5b1d68a7668a31a12b334c
IT19199986 <it19199986@my.sliit.lk>
2021-04-21 01:08:32 +0530

    Initial Commit

---
+++ funds_service/src/com/FundsService.java
@@ -0,0 +1,76 @@
+package com;
+
+import model.Funds;
+
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*; 
+
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+
+
+@Path("/Funds") 
+public class FundsService {
+
+	Funds fundsObj = new Funds(); 
+	@GET
+	@Path("/") 
+	@Produces(MediaType.TEXT_HTML) 
+	public String readFunds() 
+	 { 
+	 return fundsObj.readFunds(); 
+	 } 
+	
+	@POST
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String insertfunds(@FormParam("ProductId") String ProductId, 
+	 @FormParam("ProductName") String ProductName, 
+	 @FormParam("FName") String FName, 
+	 @FormParam("Amount") String Amount) 
+	{ 
+	 String output = fundsObj.insertfunds(ProductId, ProductName, FName, Amount); 
+	return output; 
+	}
+	
+	@PUT
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_JSON) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String updateFunds(String fundData) 
+	{ 
+	//Convert the input string to a JSON object 
+	 JsonObject fundsObject = new JsonParser().parse(fundData).getAsJsonObject(); 
+	//Read the values from the JSON object
+	 String fundID = fundsObject.get("FundID").getAsString(); 
+	 String productId = fundsObject.get("productId").getAsString(); 
+	 String productName = fundsObject.get("productName").getAsString(); 
+	 String fName = fundsObject.get("FName").getAsString(); 
+	 String amount = fundsObject.get("amount").getAsString(); 
+	 String output = fundsObj.updateFunds(fundID, productId, productName, fName, amount); 
+	return output; 
+	}
+	
+	@DELETE
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_XML) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String deleteFunds(String fundData) 
+	{ 
+	//Convert the input string to an XML document
+	 Document doc = Jsoup.parse(fundData, "", Parser.xmlParser()); 
+	 
+	//Read the value from the element <itemID>
+	 String FundID = doc.select("FundID").text(); 
+	 String output = fundsObj.deleteFunds(FundID); 
+	return output; 
+	}
+	
+}
---
+++ funds_service/src/model/Funds.java
@@ -0,0 +1,190 @@
+package model;
+
+import java.sql.*; 
+
+public class Funds {
+
+	//A common method to connect to the DB
+		 private Connection connect() 
+		 { 
+			 Connection con = null; 
+			 
+			 try
+			 { 
+				 Class.forName("com.mysql.jdbc.Driver"); 
+		 
+				 //Provide the correct details: DBServer/DBName, username, password 
+				 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/funds_service", "root", "anuwaniga#9096"); 
+			 } 
+			 catch (Exception e) 
+			 {e.printStackTrace();} 
+			 
+			 return con; 
+		 } 
+		 
+		 public String insertfunds(String ProductId, String ProductName, String FName, String Amount) 
+		 { 
+			 String output = ""; 
+			 
+			 try
+			 { 
+				 Connection con = connect(); 
+				 if (con == null) 
+				 {return "Error while connecting to the database for inserting."; }
+				 
+				 // create a prepared statement
+				 String query = " insert into funds ('FundID',`ProductId`,`ProductName`,`FName`,`Amount`)"
+						 		+ " values (?, ?, ?, ?, ?)"; 
+				 
+				 PreparedStatement preparedStmt = con.prepareStatement(query); 
+				 
+				 // binding values
+				 preparedStmt.setInt(1, 0); 
+				 preparedStmt.setString(2, ProductId); 
+				 preparedStmt.setString(3, ProductName);
+				 preparedStmt.setString(4, FName); 
+				 preparedStmt.setString(5, Amount);
+				 //preparedStmt.setDouble(5, Double.parseDouble(Amount)); 
+
+				 
+				 // execute the statement
+				 preparedStmt.execute(); 
+				 con.close(); 
+				 
+				 output = "Inserted successfully"; 
+			  } 
+			  catch (Exception e) 
+			  { 
+				 output = "Error while inserting the funds."; 
+				 System.err.println(e.getMessage()); 
+			  } 
+			 
+			  return output; 
+			} 
+		 
+			public String readFunds() 
+			{ 
+				 String output = ""; 
+				 
+				 try
+				 { 
+					 Connection con = connect();
+					 
+					 if (con == null) 
+					 {return "Error while connecting to the database for reading."; } 
+					 
+					 // Prepare the html table to be displayed
+					 output = "<table border='1'><tr><th>Product Id</th><th>Product Name</th>" +
+							  "<th>Funder's Name</th>" + 
+							  "<th>Amount</th>" +
+							  "<th>Update</th><th>Remove</th></tr>"; 
+				 
+					 String query = "select * from funds"; 
+					 Statement stmt = con.createStatement(); 
+					 ResultSet rs = stmt.executeQuery(query); 
+					 
+					 // iterate through the rows in the result set
+					 while (rs.next()) 
+					 { 
+						 String FundID = Integer.toString(rs.getInt("FundID")); 
+						 String ProductId = rs.getString("ProductId"); 
+						 String ProductName = rs.getString("ProductName"); 
+						 String FName = Double.toString(rs.getDouble("FName")); 
+						 String Amount = rs.getString("Amount"); 
+						 
+						 // Add into the html table
+						 output += "<tr><td>" + ProductId + "</td>"; 
+						 output += "<td>" + ProductName + "</td>"; 
+						 output += "<td>" + FName + "</td>"; 
+						 output += "<td>" + Amount + "</td>"; 
+						 
+						 // buttons
+						 output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
+								 	+ "<td><form method='post' action='funds.jsp'>"
+								 	+ "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
+								 	+ "<input name='FundID' type='hidden' value='" + FundID 
+								 	+ "'>" + "</form></td></tr>"; 
+				      } 
+					 
+					  con.close(); 
+					  
+					  // Complete the html table
+					  output += "</table>"; 
+				 } 
+				 catch (Exception e) 
+				 { 
+					 output = "Error while reading the Funds."; 
+					 System.err.println(e.getMessage()); 
+				 } 
+				 
+				 return output; 
+				 
+			} 
+			public String updateFunds(String FID, String proID, String ProName, String FName, String amount)
+			{ 
+				 String output = ""; 
+				 
+				 try
+				 { 
+					 Connection con = connect(); 
+					 
+					 if (con == null) 
+					 {return "Error while connecting to the database for updating."; } 
+					 
+					 // create a prepared statement
+					 String query = "UPDATE FundID SET FName=?,Amount=?"; 
+					 
+					 PreparedStatement preparedStmt = con.prepareStatement(query); 
+					 
+					 // binding values
+					 preparedStmt.setString(1, proID); 
+					 preparedStmt.setString(2, ProName); 
+					 preparedStmt.setString(3, FName); 
+					 preparedStmt.setDouble(4, Double.parseDouble(amount)); 
+					 preparedStmt.setInt(5, Integer.parseInt(FID)); 
+					 
+					 // execute the statement
+					 preparedStmt.execute(); 
+					 con.close(); 
+					 
+					 output = "Updated successfully"; 
+				 } 
+				 catch (Exception e) 
+				 { 
+					 output = "Error while updating the funds."; 
+					 System.err.println(e.getMessage()); 
+				 } 
+				 
+				 return output; 
+			} 
+			public String deleteFunds(String FundID) 
+			{ 
+				 String output = ""; 
+				 try
+				 { 
+					 Connection con = connect(); 
+					 
+					 if (con == null) 
+					 {return "Error while connecting to the database for deleting."; } 
+					 
+					 // create a prepared statement
+					 String query = "delete from funds where FundID=?"; 
+					 PreparedStatement preparedStmt = con.prepareStatement(query); 
+					 
+					 // binding values
+					 preparedStmt.setInt(1, Integer.parseInt(FundID)); 
+					 
+					 // execute the statement
+					 preparedStmt.execute(); 
+					 con.close(); 
+					 output = "Deleted successfully"; 
+				 } 
+				 catch (Exception e) 
+				 { 
+					 output = "Error while deleting the Funds."; 
+					 System.err.println(e.getMessage()); 
+				 } 
+				 return output; 
+			} 
+	
+}
