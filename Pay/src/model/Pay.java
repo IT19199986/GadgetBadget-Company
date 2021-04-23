09d69c2002abfee2375870d31eb83d872e88f9d9
1999marshal <1999Marshal>
2021-04-24 00:27:09 +0530

    Fix Pay.java class error

--- Pay/src/model/Pay.java
+++ Pay/src/model/Pay.java
@@ -9,7 +9,7 @@ public class Pay {
 	 Connection con = null; 
 	 try
 	 { 
-		 Class.forName("com.mysql.jdbc.Dr"); 
+		 Class.forName("com.mysql.jdbc.Driver"); 
 		 //Provide the correct details: DBServer/DBName, username, password 
 		 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/pafcus", "root", ""); 
 	 } 
