150e8111e98e7d03c703380d44cd7cde86bb6944
IT19199986 <68660619+IT19199986@users.noreply.github.com>
2021-04-25 11:20:49 +0530

    Add files via upload
    Database, Diagrams, Test Images added

---
+++ Funds Service(IT19199986)/MYSQL Database/Dump20210425.sql
@@ -0,0 +1,89 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: funds_service
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `funding_body`
+--
+
+DROP TABLE IF EXISTS `funding_body`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `funding_body` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `FName` varchar(45) NOT NULL,
+  `Address` varchar(100) DEFAULT NULL,
+  `Email` varchar(45) NOT NULL,
+  `Phone` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funding_body`
+--
+
+LOCK TABLES `funding_body` WRITE;
+/*!40000 ALTER TABLE `funding_body` DISABLE KEYS */;
+INSERT INTO `funding_body` VALUES (1,'Mr. De Sliva','Colombo 5','DeSliva@gmail.com','0714762897'),(2,'Mrs. Narmada','Matara','Narmada77@gmail.com','0776543217'),(5,'Mr. Anuradha','No.17/A, Nation road, Kandy.','Anuradha78@gmail.com','0765420969');
+/*!40000 ALTER TABLE `funding_body` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `funds`
+--
+
+DROP TABLE IF EXISTS `funds`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `funds` (
+  `FundID` int NOT NULL AUTO_INCREMENT,
+  `ProductId` varchar(20) NOT NULL,
+  `ProductName` varchar(30) NOT NULL,
+  `FName` varchar(45) NOT NULL,
+  `Amount` varchar(20) NOT NULL,
+  PRIMARY KEY (`FundID`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funds`
+--
+
+LOCK TABLES `funds` WRITE;
+/*!40000 ALTER TABLE `funds` DISABLE KEYS */;
+INSERT INTO `funds` VALUES (5,'P002','Product 2','Anudi','15000.00'),(7,'P005','Product 5','Nimesh','50000.00'),(8,'P001','Product 1','MR.Nirmal','30000.00'),(9,'P001','Product 1','Mr. Wijethunga','100000.00'),(10,'P004','Product 4','Mr. Mendis','100000.00');
+/*!40000 ALTER TABLE `funds` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'funds_service'
+--
+
+--
+-- Dumping routines for database 'funds_service'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-25 11:06:48
